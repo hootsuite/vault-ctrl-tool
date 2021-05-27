52d64b6c458ae16d36716f8c178224f602ca1f02
tom-hadlaw-hs <thomas.hadlaw@hootsuite.com>
2021-05-27 15:33:35 -0700

    return error correctly

--- vaulttoken/vault_token.go
+++ vaulttoken/vault_token.go
@@ -3,9 +3,10 @@ package vaulttoken
 import (
 	"errors"
 	"fmt"
-	"github.com/hootsuite/vault-ctrl-tool/v2/util"
 	"os"
 	"strconv"
+
+	"github.com/hootsuite/vault-ctrl-tool/v2/util"
 
 	"github.com/hashicorp/vault/api"
 	"github.com/hootsuite/vault-ctrl-tool/v2/briefcase"
@@ -85,7 +86,7 @@ func (vt *vaultTokenManager) Set(authToken *util.WrappedToken) error {
 	return nil
 }
 
-// Looks for a valid Vault token and will extend it out if it's going to expire soon. The extension is just long
+// CheckAndRefresh looks for a valid Vault token and will extend it out if it's going to expire soon. The extension is just long
 // enough to use it for things. Returns ErrNoValidVaultTokenAvailable if none is available, or different errors
 // if something goes wrong along the way.
 func (vt *vaultTokenManager) CheckAndRefresh() error {
